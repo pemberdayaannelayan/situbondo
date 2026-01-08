7f3aaa03ce423a8af500ce4c4dee59d71a5b003f
pemberdayaan nelayan <disnakansitubondo@gmail.com>
2026-01-08 19:32:27 +0700

    Create paowan.js

---
+++ simpadan/paowan.js
@@ -0,0 +1,34 @@
+// DATA NELAYAN GLOBAL - SISTEM PEMETAAN NELAYAN
+// File: paowan.js
+// Dibuat: 8/1/2026, 19.26.03
+// Jumlah Data: 
+
+window.SIMATA_BACKUP_DATA = [
+  {
+    "id": 1767875263779.1787,
+    "nama": "AANG KURNIAWAN",
+    "nik": "3512040305020001",
+    "whatsapp": "085230841931",
+    "profesi": "Nelayan Penuh Waktu",
+    "status": "Anak Buah Kapal",
+    "tahunLahir": 1953,
+    "usia": 73,
+    "kecamatan": "Panarukan",
+    "desa": "Paowan",
+    "alatTangkap": "Pukat Tarik",
+    "namaKapal": "-",
+    "jenisKapal": "-",
+    "jenisIkan": "Ikan Barakuda, Ikan Layur, Ikan Kurisi, Ikan Cakalang",
+    "usahaSampingan": "",
+    "tanggalValidasi": "2025-12-23",
+    "validator": "MUHAMMAD S",
+    "driveLink": "",
+    "kodeValidasi": "VLD-JE7FCK",
+    "keterangan": "TERDAFTAR KUSUKA",
+    "importDate": "2026-01-08T12:27:41.567Z",
+    "missingFields": [],
+    "isComplete": true,
+    "createdAt": "2026-01-08T12:27:43.779Z",
+    "createdBy": "System Import"
+  }
+];
