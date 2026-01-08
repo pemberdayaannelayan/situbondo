2d09ddb7269320418c91570879dbfec973bbb240
pemberdayaan nelayan <disnakansitubondo@gmail.com>
2026-01-08 19:31:25 +0700

    Update wringinanompanarukan.js

--- simpadan/wringinanompanarukan.js
+++ simpadan/wringinanompanarukan.js
@@ -1,37 +1,10 @@
 // DATA NELAYAN GLOBAL - SISTEM PEMETAAN NELAYAN
 // File: wringinanompanarukan.js
 // Dibuat: 8/1/2026, 19.28.03
-// Jumlah Data: 7
+// Jumlah Data: 6
 
 window.SIMATA_BACKUP_DATA = [
-  {
-    "id": 1767875263779.1787,
-    "nama": "AANG KURNIAWAN",
-    "nik": "3512040305020001",
-    "whatsapp": "085230841931",
-    "profesi": "Nelayan Penuh Waktu",
-    "status": "Anak Buah Kapal",
-    "tahunLahir": 1953,
-    "usia": 73,
-    "kecamatan": "Panarukan",
-    "desa": "Paowan",
-    "alatTangkap": "Pukat Tarik",
-    "namaKapal": "-",
-    "jenisKapal": "-",
-    "jenisIkan": "Ikan Barakuda, Ikan Layur, Ikan Kurisi, Ikan Cakalang",
-    "usahaSampingan": "",
-    "tanggalValidasi": "2025-12-23",
-    "validator": "MUHAMMAD S",
-    "driveLink": "",
-    "kodeValidasi": "VLD-JE7FCK",
-    "keterangan": "TERDAFTAR KUSUKA",
-    "importDate": "2026-01-08T12:27:41.567Z",
-    "missingFields": [],
-    "isComplete": true,
-    "createdAt": "2026-01-08T12:27:43.779Z",
-    "createdBy": "System Import"
-  },
-  {
+    {
     "id": 1767875263779.6584,
     "nama": "MOH. ABI BAI SUNI",
     "nik": "3512062212970001",
